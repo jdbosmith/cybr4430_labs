c990bdd1cb700b76aca79bfe55181ffbc7897c07
Greg Hoff <31416820+GregHoff@users.noreply.github.com>
2025-09-28 20:20:43 -0500

    Initial commit

---
+++ mod01_07.py
@@ -0,0 +1,9 @@
+#  function that takes a well-formed matrix with real/complex entries as its input and outputs true if the input matrix is Hermitian and outputs false if the matrix is not Hermitian.
+def isHermitian(m):
+    #Complete the function
+    return False
+    
+#  function takes a well-formed matrix with real/complex entries as its input and outputs true if the input matrix is unitary and outputs false if the input matrix is not unitary.
+def isUnitary(m):
+    #Complete the function
+    return False
