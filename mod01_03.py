bc2f598e41e1765baf6c381dc6a2884374f6da49
Greg Hoff <31416820+GregHoff@users.noreply.github.com>
2025-09-28 19:33:54 -0500

    Update mod01_03.py

---
+++ mod01_03.py
@@ -0,0 +1,14 @@
+#  function that takes a well formed complex number in cartesian form as its input and returns the polar representation of its input.
+def toPolar(c1):
+    #Complete the function
+    return False
+    
+#  function that takes a well formed polar representation of a complex number as its input and returns the cartesian representation of its input.
+def toCart(mag, angle):
+    #Complete the function
+    return False
+    
+# function that takes two complex numbers in polar form as its inputs and returns the product of its two inputs in polar form.
+def multPolar(m1, a1, m2, a2):
+    #Complete the function
+    return False
