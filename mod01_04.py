3f0222db6371205dbc3fa926047415b223fa0fdd
Greg Hoff <31416820+GregHoff@users.noreply.github.com>
2025-09-28 20:06:03 -0500

    Initial commit

---
+++ mod01_04.py
@@ -0,0 +1,24 @@
+#  function that takes two equal dimensioned complex vectors/matrices as its inputs and returns a vector/matrix that is obtained by adding its two inputs.
+def matAdd(m1,m2):
+    #Complete the function
+    return False
+    
+#  function that takes a scalar and a complex vector/matrix as its two inputs and returns a vector/matrix that is a product of its two inputs.
+def scalarMatMult(m1,m2):
+    #Complete the function
+    return False
+    
+# function that takes a well-formed matrix/vector as its input and returns the transpose of its input.
+def transpose(m1):
+    #Complete the function
+    return False
+
+# function that takes a well formed matrix/vector as its input and returns the conjugate of its input.
+def conjugate(m1):
+    #Complete the function
+    return False
+
+# function that applies the dagger operation on a matrix/vector and returns the result.
+def conjugTranspose(m1):
+    #Complete the function
+    return False
