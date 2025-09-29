999ac19643de232022baa32ea174ff68ba89bbc2
Greg Hoff <31416820+GregHoff@users.noreply.github.com>
2025-09-28 20:22:26 -0500

    Update mod01_07.py

--- mod01_07.py
+++ mod01_07.py
@@ -1,9 +1,4 @@
-#  function that takes a well-formed matrix with real/complex entries as its input and outputs true if the input matrix is Hermitian and outputs false if the matrix is not Hermitian.
-def isHermitian(m):
-    #Complete the function
-    return False
-    
-#  function takes a well-formed matrix with real/complex entries as its input and outputs true if the input matrix is unitary and outputs false if the input matrix is not unitary.
-def isUnitary(m):
+#  function that take two well-formed matrices with real/complex entries and outputs the tensor product of is inputs.
+def tensor(m1,m2):
     #Complete the function
     return False
