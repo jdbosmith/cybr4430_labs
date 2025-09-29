c9b8187669851c256d1746ab557fe0a919ba0739
Greg Hoff <31416820+GregHoff@users.noreply.github.com>
2025-09-28 20:23:59 -0500

    Update test_mod01_08.py

--- tests/test_mod01_07.py
+++
@@ -1,9 +0,0 @@
-from mod01_07 import isHermitian, isUnitary
-import numpy
-
-def test_isHermitian():   
-    assert isHermitian([[1,2-1j],[2 + 1j, 0]]) == True
-    
-def test_isUnitary():
-    assert isUnitary([[.5+.5j,.5+.5j],[.5-.5j,-.5 + .5j]]) == True
-    
