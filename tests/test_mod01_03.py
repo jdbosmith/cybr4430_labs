b83325fb6ed795619a10a459a7f6c99a512e9f8a
Greg Hoff <31416820+GregHoff@users.noreply.github.com>
2025-09-30 18:41:49 -0500

    Fixed bad function reference

--- tests/test_mod01_03.py
+++ tests/test_mod01_03.py
@@ -1,12 +1,11 @@
 from mod01_03 import toPolar, toCart, multPolar
-import math
-import cmath
+from math import isclose
 
 def test_toPolar():   
     assert toPolar(-6) == (6,180)
 
 def test_toCart():
-    assert math.isclose(toCart(3.6055512754639896,56.309932474020215).real, 2) and math.isclose(toCart(3.6055512754639896,56.309932474020215).imag, 3)
+    assert isclose(toCart(3.6055512754639896,56.309932474020215).real, 2) and isclose(toCart(3.6055512754639896,56.309932474020215).imag, 3)
 
 def test_multPolar():
     assert isclose(multPolar(5, 30, 4, -45)[0],20) and isclose(multPolar(5, 30, 4, -45)[1],-15)
