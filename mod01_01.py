37631fb445cd0f7f86f94ebf03c935ff3cc5f266
Jason Smith <j.dbo.smith@gmail.com>
2026-08-26 18:13:05 -0500

    Initial commit

---
+++ mod01_01.py
@@ -0,0 +1,19 @@
+# function that takes two well-formed complex numbers as its inputs and outputs their sum as a well-formed complex number
+def sumComplex(num1,num2):
+    #Complete the function by returning num1 + num2
+    return False
+    
+# function that takes two well-formed complex numbers as its inputs and outputs their product as a well-formed complex number.
+def productComplex(num1,num2):
+    #Complete the function by returning num1 * num2
+    return False
+    
+# function that takes a well-formed complex number as its input and outputs its real component.
+def getReal(num):
+    #Complete the function by returning num.real
+    return False
+
+# function that takes a well-formed complex number as its input and outputs its imaginary component.
+def getImaginary(num):
+    #Complete the function by returning num.imag
+    return False
