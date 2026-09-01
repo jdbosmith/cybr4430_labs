ab503ff14400ffdab0c6202f677ab7c1e31ffb94
Jason Smith <j.dbo.smith@gmail.com>
2026-08-31 19:27:14 -0500

    divComplex method

--- mod01_02.py
+++ mod01_02.py
@@ -1,8 +1,14 @@
 # function that takes any two well-formed complex numbers num1 numerator and num2 denominator as inputs and outputs the division as a well-formed complex number
 def divComplex(num1,num2):
     #Complete the function by returning num1 / num2
-    return False
+    a, b = num1.real, num1.imag
+    c, d = num2.real, num2.imag
+    denominator = c*c + d*d
+    real = (a * c + b * d) / denominator
+    imaginary = (b * c - a * d) / denominator
     
+    return real + imaginary*1j   
+
 # function that takes a well-formed complex number num1 as its input and outputs modulus (absolute value) as a positive real number.
 def modComplex(num1):
     #Complete the function by returning the modulus (absolute value)
