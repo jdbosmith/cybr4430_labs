f9bbc16fd8a4b16d8edb2e61464e63c1fd256650
Jason Smith <j.dbo.smith@gmail.com>
2026-08-31 19:43:17 -0500

    additional two functions

--- mod01_02.py
+++ mod01_02.py
@@ -6,15 +6,21 @@ def divComplex(num1,num2):
     denominator = c*c + d*d
     real = (a * c + b * d) / denominator
     imaginary = (b * c - a * d) / denominator
-    
+
     return real + imaginary*1j   
 
 # function that takes a well-formed complex number num1 as its input and outputs modulus (absolute value) as a positive real number.
 def modComplex(num1):
     #Complete the function by returning the modulus (absolute value)
-    return False
+    a, b = num1.real, num1.imag
+    modulus = (a**2 + b**2)**0.5
+
+    return modulus  
     
 # function that takes a well-formed complex number num 1as its input and outputs and outputs the conjugate of the number.
 def conjComplex(num):
     #Complete the function by returning the conjugate
-    return False
+    a, b = num.real, num.imag
+    conjugate = a - b*1j
+    
+    return conjugate
